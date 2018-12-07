23285fe6eb1ce5f26d927077c225a3edb7f4f0f1
duxiangyu <hnyd1995@163.com>
2018-12-07 14:22:14 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,29 @@
 /**
  * Created by Captain on 2018/12/6 14:38.
  */
-export default {
-  input: 'src/main.js',
-  output: {
-    file: 'dist/traffic.js',
-    format: 'es'
+export default [
+  {
+    file: 'dist/traffic.cjs.js',
+    format: 'cjs'
+  }, {
+    file: 'dist/traffic.esm.js',
+    format: 'esm'
+  }, {
+    file: 'dist/traffic.umd.js',
+    format: 'umd',
+    name: 'traffic'
+  }, {
+    file: 'dist/traffic.amd.js',
+    format: 'amd'
+  }, {
+    file: 'dist/traffic.iife.js',
+    format: 'iife',
+    name: 'traffic'
   }
-};
+].map(output => ({
+  input: 'src/main.js',
+  output,
+  plugins: [
+
+  ]
+}));
