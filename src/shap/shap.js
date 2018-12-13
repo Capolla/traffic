89622258407f25f87b70e24ded28c82985b5f770
duxiangyu <hnyd1995@163.com>
2018-12-13 22:16:11 +0800

    add shap base class

---
+++ src/shap/shap.js
@@ -0,0 +1,22 @@
+/**
+ * Shap base class
+ */
+class Shap {
+  constructor () {
+    /**
+     * 横坐标
+     */
+    this.x = 0
+    /**
+     * 纵坐标
+     */
+    this.y = 0
+    /**
+     * 是否可用
+     */
+    this.active = true
+    // TODO
+  }
+}
+
+export default Shap
