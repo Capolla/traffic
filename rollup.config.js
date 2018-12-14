e1ca5344953b43c8feef32337607e87ea704fbfb
duxiangyu <hnyd1995@163.com>
2018-12-14 10:28:38 +0800

    add resolve, commonjs, replace

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
 
 export default [
   {
@@ -23,8 +26,19 @@ export default [
   input: 'src/main.js',
   output,
   plugins: [
+    // rollup-plugin-node-resolve 插件使rollup允许加载node_modules中的第三方模块
+    resolve({
+      jsnext: true, // 将 Node 包转换为 ES2015 模块
+      main: true,
+      browser: true
+    }),
+    // rollup-plugin-commonjs 插件会将CommonJS模块转换为ES6,使得rollup打包兼容
+    commonjs(),
     babel({
       exclude: 'node_modules/**' // babel仅仅编译项目的源码
+    }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development') // 更换全局变量ENV的值
     })
   ]
 }))
