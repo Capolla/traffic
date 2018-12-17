7aeaeaf2589db67004b508d215610ca857e362c8
duxiangyu <hnyd1995@163.com>
2018-12-17 09:39:03 +0800

    rebuild shap and rect

--- src/shap/rect.js
+++ src/shap/rect.js
@@ -1,13 +1,26 @@
 import Shap from './shap'
+import constant from '../util/constant'
 
 /**
  * Rect class
  */
 class Rect extends Shap {
-  constructor (name) {
-    super()
-    this.type = 'rect'
-    this.name = name
+  constructor (name = 'rect', property = {}) {
+    super(name, constant.RECT)
+    /**
+     * 宽
+     */
+    this.width = 0
+    /**
+     * 高
+     */
+    this.height = 0
+
+    this.color = property['color'] || '#000000'
+    this.coorX = property['coorX'] || 0
+    this.coorY = property['coorY'] || 0
+    this.width = property['width'] || 0
+    this.height = property['height'] || 0
   }
 }
 
--- src/shap/shap.js
+++ src/shap/shap.js
@@ -4,20 +4,43 @@
  * traffic中所有形状的基类
  */
 class Shap {
-  constructor () {
+  constructor (name = 'shpe', type) {
+    /**
+     * id
+     */
+    this.index = -1
+    /**
+     * 名称
+     */
+    this.name = name
     /**
      * 横坐标
      */
-    this.x = 0
+    this.coorX = 0
     /**
      * 纵坐标
      */
-    this.y = 0
+    this.coorY = 0
+    /**
+     * 状态
+     */
+    this.status = 'normal'
     /**
      * 是否可用
      */
-    this.active = true
-    // TODO
+    this.isActive = true
+    /**
+     * 类型
+     */
+    this.type = type
+    /**
+     * 颜色
+     */
+    this.color = '#000000'
+    /**
+     * 引用index
+     */
+    this.refIndex = -1
   }
 }
 
