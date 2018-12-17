16edd7fe6a226c66c7466337cf42407e0b2f5e10
duxiangyu <hnyd1995@163.com>
2018-12-17 11:42:53 +0800

    remove detail draw options from Brush to specific Shape

--- src/core/brush.js
+++ src/core/brush.js
@@ -1,48 +1,64 @@
-import Rect from '../shap/rect'
-import debug from 'debug'
-
-const logger = debug('app:log')
+import BaseClass from './base'
+import constant from '../util/constant'
+import model from '../model/model'
 
 /**
  * Brush画笔
  *
  * 对html5 canvas api的封装，负责traffic的绘制工作
  */
-class Brush {
+class Brush extends BaseClass {
   constructor (canvasId) {
+    super()
+
     /**
      * html5 canvas
      */
     this.canvas = document.getElementById(canvasId).getContext('2d')
-
-    this.shapList = []
   }
   /**
    * 绘制图形
    * @param {Shap} shap
    */
   draw (shap) {
-    logger('draw shap', shap)
+    var shapeAllList = model.get(constant.SHAPE_ALL)
+    shapeAllList.push(shap)
+    model.set(constant.SHAPE_ALL, shapeAllList)
 
-    this.shapList.push(shap)
-
-    if (shap instanceof Rect) {
-      this.drawRect(shap)
-    } else {
-      logger('unkonw shap')
-    }
+    this.logger('draw shap', shap)
+    shap.draw(this.canvas)
   }
+
   /**
-   * 绘制矩形
-   * @param {Rect} rect
+   * 绘制shapList
    */
-  drawRect (rect) {
-    logger('draw rect', rect)
+  drawAll () {
+    this.logger('--- draw all shap ---')
 
-    this.canvas.lineWidth = 30
-    this.canvas.fillStyle = rect.color
-    this.canvas.fillRect(rect.coorX, rect.coorY, rect.width, rect.height)
+    var shapeAllList = model.get(constant.SHAPE_ALL)
+    shapeAllList.array.forEach(shap => {
+      shap.draw(this.canvas)
+    })
   }
+
+  /**
+   * 返回html5 canvas
+   * @return {Canvas} canvas
+   */
+  h5Canvas () {
+    return this.canvas
+  }
+  // /**
+  //  * 绘制矩形
+  //  * @param {Rect} rect
+  //  */
+  // drawRect (rect) {
+  //   this.logger('draw rect', rect)
+
+  //   this.canvas.lineWidth = 30
+  //   this.canvas.fillStyle = rect.color
+  //   this.canvas.fillRect(rect.coorX, rect.coorY, rect.width, rect.height)
+  // }
 }
 
 export default Brush
