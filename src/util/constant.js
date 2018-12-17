8921f0713c0741fefa79de1b093c9de05d5635be
duxiangyu <hnyd1995@163.com>
2018-12-17 09:40:02 +0800

    add some constant

--- src/util/constant.js
+++ src/util/constant.js
@@ -5,5 +5,9 @@ const constant = {}
 
 constant.TRAFFIC = 'traffic'
 constant.CANVAS = 'canvas'
+constant.BRUSH = 'brush'
+constant.LOGGER = 'logger'
+
+constant.RECT = 'shap_rect'
 
 export default constant
