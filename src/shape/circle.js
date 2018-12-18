e837950b48293e960f79c6a98951ed51e51516a3
XiangyuDu <hnyd1995@163.com>
2018-12-18 15:10:13 +0800

    add shape circle

---
+++ src/shape/circle.js
@@ -0,0 +1,37 @@
+import Shape from './shape'
+import constant from '../util/constant'
+
+/**
+ * 圆形
+ */
+class Circle extends Shape {
+  constructor (name = 'circle', property = {}) {
+    super(name, constant.SHAPE_CIRCLE, Circle.name)
+
+    /**
+     * 半径
+     */
+    this.radius = property['radius'] || 0
+
+    this.color = property['color'] || '#000000'
+    this.coorX = property['coorX'] || 0
+    this.coorY = property['coorY'] || 0
+  }
+
+  /**
+   * draw circle with html5 canvas
+   *
+   * @param {Canvas} canvas
+   * @override
+   */
+  draw (canvas) {
+    super.draw(this)
+
+    canvas.beginPath()
+    canvas.fillStyle = this.color
+    canvas.arc(this.coorX, this.coorY, this.radius, 0, Math.PI * 2)
+    canvas.fill()
+  }
+}
+
+export default Circle
