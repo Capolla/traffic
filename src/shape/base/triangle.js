d24751597401f8d756afe0ea3b459aac51d70c6c
XiangyuDu <hnyd1995@163.com>
2018-12-18 15:49:15 +0800

    add shape triangle

---
+++ src/shape/base/triangle.js
@@ -0,0 +1,40 @@
+import Shape from './shape'
+import constant from '../../util/constant'
+
+/**
+ * 三角形
+ */
+class Triangle extends Shape {
+  constructor (name = 'triangle', property = {}) {
+    super(name, constant.SHAPE_TRIANGLE, Triangle.name)
+
+    /**
+     * x坐标列表：以顶点为起始，沿顺时针方向依次三个顶点的x坐标
+     */
+    this.xList = property['xList'] || [0, 0, 0]
+    /**
+     * y坐标列表：以顶点为起始，沿顺时针方向依次三个顶点的y坐标
+     */
+    this.yList = property['yList'] || [0, 0, 0]
+    this.color = property['color'] || '#000000'
+  }
+
+  /**
+   * draw triangle with html5 canvas
+   *
+   * @param {Canvas} canvas
+   * @override
+   */
+  draw (canvas) {
+    super.draw(this)
+
+    canvas.beginPath()
+    canvas.moveTo(this.xList[0], this.yList[0])
+    canvas.lineTo(this.xList[1], this.yList[1])
+    canvas.lineTo(this.xList[2], this.yList[2])
+    canvas.fillStyle = this.color
+    canvas.fill()
+  }
+}
+
+export default Triangle
